f0f0f9182978c7b7914f8dcbb9db4e03c2534802
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2025-02-10 23:42:02 +0530

    API building

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,11 +3,21 @@ const { userAuth } = require("../middlewares/auth");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
+requestRouter.post(
+  "/request/send/interested/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;  // from userId
+      const toUserID = req.params.toUserId;
+      const status = req.params.toUserId;
+      
+    } catch (error) {
+      res.status(400).send("ERROR: " + error.message);
+    }
 
-  console.log("Sending a connection request");
-  res.send(user.firstName + " Send Connection request");
-});
+    res.send(user.firstName + " Send Connection request");
+  }
+);
 
 module.exports = requestRouter;
